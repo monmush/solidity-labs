2096111dbf8d838b5e36358df89287705722ba28
monmush <omiph001@gmail.com>
2024-08-10 15:10:46 +0700

    First commit

---
+++ contracts/Counter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter { 
+    uint count;
+
+    constructor() {
+        count = 0;
+    }
+
+    // Read function
+    function getCount () public view returns(uint) {
+        return count;
+    }
+
+    // Write function
+    function incrementCount() public {
+        count = count + 1;
+    }
+} 
