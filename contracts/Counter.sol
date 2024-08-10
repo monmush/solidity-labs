8d0293486e3e39bf157bafaff9de56e9ca31950a
monmush <omiph001@gmail.com>
2024-08-10 15:33:04 +0700

    Data types

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Counter { 
-    uint count;
-
-    constructor() {
-        count = 0;
-    }
+    uint public count = 0;
 
     // Read function
     function getCount () public view returns(uint) {
