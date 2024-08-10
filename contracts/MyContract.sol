2d3f0a05202fca686cc81ee58bc4412bb312530e
monmush <omiph001@gmail.com>
2024-08-10 21:40:28 +0700

    Mapping

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,44 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  ^0.8.0;
 
+// contract MyContract { 
+//     // State Variables - can be accessed in entire contract
+//     uint public myUint = 1;
+//     uint256 public myUint256 = 1;
+//     uint8 public myUint8 = 1;
+//     int public myInt = 1;
+
+//     string public myString = "Hello, world!";
+//     bytes32 public myBytes32 = "Hello, world!";
+
+//     address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+//     // Create our own data structure
+//     struct MyStruct {
+//         uint256 myUint256;
+//         string myString;
+//     }
+
+//     MyStruct public myStruct = MyStruct(1, "Hello, world!");
+
+//     // Array
+//     uint[] public uintArray = [1, 2, 3];
+
+//     // 2D array
+//     uint256[][] public array2D = [[1,2,3], [4,5,6]];
+
+//     // Local Variables - can be accessed only in the scope of function
+//     // pure means that this function doesn't mutate anything outside its scope
+//     function getValue() public pure returns (uint) { 
+//         uint value = 1;
+//         return value;
+//     }
+
+//     function addValue (uint _value) public {
+//         uintArray.push(_value);
+//     }
+
+//     function valueCount() public view returns (uint){
+//         return uintArray.length;
+//     }
+// }
+
 contract MyContract { 
-    // State Variables - can be accessed in entire contract
-    uint public myUint = 1;
-    uint256 public myUint256 = 1;
-    uint8 public myUint8 = 1;
-    int public myInt = 1;
+    // Mapping
+    mapping(uint => string) names;
+    mapping(uint => Book) public books;
+    // Nested mapping 
+    mapping(address => mapping(uint => Book)) public myBooks;
 
-    string public myString = "Hello, world!";
-    bytes32 public myBytes32 = "Hello, world!";
-
-    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-
-    // Create our own data structure
-    struct MyStruct {
-        uint256 myUint256;
-        string myString;
+    struct Book {
+        string title;
+        string author;
     }
 
-    MyStruct public myStruct = MyStruct(1, "Hello, world!");
+    constructor() {
+        names[1] = "Minh";
+        names[2] = "Meo";
+    } 
 
-    // Array
-    uint[] public uintArray = [1, 2, 3];
-
-    // 2D array
-    uint256[][] public array2D = [[1,2,3], [4,5,6]];
-
-    // Local Variables - can be accessed only in the scope of function
-    // pure means that this function doesn't mutate anything outside its scope
-    function getValue() public pure returns (uint) { 
-        uint value = 1;
-        return value;
+    function addBook(uint _id, string memory _title, string memory _author) public {
+        books[_id] = Book(_title, _author);
     }
-
-    function addValue (uint _value) public {
-        uintArray.push(_value);
-    }
-
-    function valueCount() public view returns (uint){
-        return uintArray.length;
+    
+    function addMyBook(uint _id, string memory _title, string memory _author) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
     }
 }
